0e7817ee33a6924f3629c29a4ecbce0f8c2d9d57
Kordel Latimer <latimer.kordel@gmail.com>
2019-11-17 23:08:29 -0500

    Update webpack for production

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import Routes from "./pages/Routes";
 import TV from "./pages/TV";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import _ from "lodash";
+
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
 
 const initialState = {
   channelName: "GeauxInfo",
@@ -33,4 +38,12 @@ const App = () => (
   </Provider>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.body.appendChild(component()));
+
+function component() {
+  const element = document.createElement("div");
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(<App />, " ");
+
+  return element;
+}
---
+++ webpack.common.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const pkg = require("./package.json");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: pkg.babel.presets,
+              compact: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader"
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Production"
+    })
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist/src/")
+  }
+};
